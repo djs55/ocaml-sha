0dc417c05a297f50e307b1ae780c2e5306ef7c46
Vincent Hanquez <vincent@snarc.org>
2006-09-02 18:58:35 +0100

    add some 64 bits operations

--- op.h
+++ op.h
@@ -10,11 +10,14 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * SHA1 implementation as describe in wikipedia.
+ * SHA implementation low level operation
  */
 
 #ifndef SHA_OP_H
 #define SHA_OP_H
+
+typedef unsigned long long u64;
+
 
 static inline unsigned int rol32(unsigned int word, unsigned int shift)
 {
@@ -24,6 +27,16 @@ static inline unsigned int rol32(unsigned int word, unsigned int shift)
 static inline unsigned int ror32(unsigned int word, unsigned int shift)
 {
 	return (word >> shift) | (word << (32 - shift));
+}
+
+static inline u64 rol64(u64 word, unsigned int shift)
+{
+        return (word << shift) | (word >> (64 - shift));
+}
+
+static inline u64 ror64(u64 word, unsigned int shift)
+{
+	return (word >> shift) | (word << (64 - shift));
 }
 
 #if (defined(__i386__) || defined(__x86_64__)) && !defined(NO_INLINE_ASM)
@@ -39,14 +52,32 @@ static inline unsigned int swap32(unsigned int a)
 }
 #endif
 
+#if && defined(__x86_64__) && !defined(NO_INLINE_ASM)
+static inline u64 swap64(u64 a)
+{
+	asm ("bswap %0" : "=r" (a) : "0" (a));
+	return a;
+}
+#else
+static inline u64 swap64(u64 a)
+{
+	return ((u64) swap32((unsigned int) (a >> 32))) |
+	       (((u64) swap32((unsigned int) a)) << 32);
+}
+#endif
+
 /* big endian to cpu */
 #include <endian.h>
 #if LITTLE_ENDIAN == BYTE_ORDER
 #define be32_to_cpu(a) swap32(a)
 #define cpu_to_be32(a) swap32(a)
+#define be64_to_cpu(a) swap64(a)
+#define cpu_to_be64(a) swap64(a)
 #elif BIG_ENDIAN == BYTE_ORDER
 #define be32_to_cpu(a) (a)
 #define cpu_to_be32(a) (a)
+#define be64_to_cpu(a) (a)
+#define cpu_to_be64(a) (a)
 #else
 #error "endian not supported"
 #endif
