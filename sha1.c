a6e96892a6f8d9a12719e9a3ef45ef925b5781d5
Vincent Hanquez <vincent@snarc.org>
2009-11-09 10:16:50 +0000

    relicense to lgpl v2.1 or v3.0

--- sha1.c
+++ sha1.c
@@ -1,9 +1,9 @@
 /*
- *	Copyright (C) 2006-2008 Vincent Hanquez <tab@snarc.org>
+ *	Copyright (C) 2006-2009 Vincent Hanquez <tab@snarc.org>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; version 2 only.
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 or version 3.0 only.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
