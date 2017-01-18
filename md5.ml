c15de0cf4dcfbac5f4cf5f45d553b62079c03fd7
Christopher Zimmermann <madroach@gmerlin.de>
2017-01-18 20:54:50 +0100

    Add md5 digest

---
+++ md5.ml
@@ -0,0 +1,11 @@
+include Hash.Make (struct
+    type ctx
+    type t = string
+    let digest_length = 128
+
+    external to_bin: t -> string = "%identity"
+    external init: unit -> ctx = "stub_md5_init"
+    external unsafe_update_substring: ctx -> string -> int -> int -> unit = "stub_md5_update"
+    external update_buffer: ctx -> Hash.buf -> unit = "stub_md5_update_bigarray"
+    external finalize: ctx -> t = "stub_md5_finalize"
+  end)
