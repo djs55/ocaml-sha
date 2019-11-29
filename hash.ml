d218a42a2e0f0136d5ff656526cfce3314307d87
Christopher Zimmermann <madroach@gmerlin.de>
2019-11-29 19:34:18 +0100

    fix building on Ocaml > 4.07

--- hash.ml
+++ hash.ml
@@ -163,7 +163,7 @@ struct
     while (!left == -1 || !left > 0) && not !eof
     do
       let len = if !left < 0 then blksize else (min !left blksize) in
-      let readed = Pervasives.input chan buf 0 len in
+      let readed = input chan buf 0 len in
       if readed = 0 then
         eof := true
       else begin
